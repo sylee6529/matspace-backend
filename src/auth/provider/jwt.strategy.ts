2fc6750a59de18668025cadd96a8e822c2df5b66
sylee <sylee6529@gmail.com>
2024-01-29 21:09:25 +0900

    feat: jwt strategy 작성 (#1)

---
+++ src/auth/provider/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { AuthService } from "../auth.service";
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private readonly authService: AuthService
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: process.env.JWT_SECRET
+        });
+    }
+
+    async validate(payload: any) {
+        const user = await this.authService.validate(payload.sub);
+        if (!user) {
+            throw new Error('잘못된 요청입니다.');
+        }
+        return user;
+    }
+}
