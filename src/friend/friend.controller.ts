1c33a212972fb6603f6246dc4d18707192d3436e
sylee <sylee6529@gmail.com>
2024-01-31 07:40:10 +0900

    feat: 친구 초대, 수락, 거절 구현

---
+++ src/friend/friend.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateFriendInvitationDto } from './dto/create.friend.invitation.dto';
+import { FriendService } from './friend.service';
+import { GetUserId } from 'src/util/decorator/get-user.decorator';
+
+@Controller('api/friends')
+export class FriendController {
+    constructor(
+        private readonly friendnService: FriendService,
+      ) {}
+    
+      @UseGuards(AuthGuard('jwt'))
+      @Post('invite')
+      invite(@GetUserId() userId, @Body() createFriendInvitationDto: CreateFriendInvitationDto): Promise<string> {
+        return this.friendnService.invite(userId, createFriendInvitationDto);
+      }
+
+      @UseGuards(AuthGuard('jwt'))
+      @Post('accept')
+      accept(@GetUserId() userId, @Body() createFriendInvitationDto: CreateFriendInvitationDto): Promise<string> {
+        return this.friendnService.accept(userId, createFriendInvitationDto);
+      }
+
+      @UseGuards(AuthGuard('jwt'))
+      @Post('reject')
+      reject(@GetUserId() userId, @Body() createFriendInvitationDto: CreateFriendInvitationDto): Promise<string> {
+        return this.friendnService.reject(userId, createFriendInvitationDto);
+      }
+}
