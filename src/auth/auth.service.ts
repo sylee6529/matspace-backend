02f2568b43bbe1eb8da16575dafe4c9d4a5c87e0
sylee <sylee6529@gmail.com>
2024-01-31 07:41:51 +0900

    fix: _id 필드 추가 후 관련 코드 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,24 +7,30 @@ import { hash, isHashValid } from './../util/bcrypt.encoder';
 import { JwtService } from '@nestjs/jwt';
 import { UserResponseDto } from './dto/response/user.response.dto';
 import { LoginRequestDto } from './dto/request/login.request.dto';
+import { ConfigService } from '@nestjs/config';
+import { IUser } from './interface/user.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectModel(User.name) private readonly userModel: Model<User>,
-        private readonly jwtService: JwtService) {}
+        private readonly jwtService: JwtService,
+        private readonly configService: ConfigService,
+        ) {}
 
     async create(registerRequestDto: RegisterRequestDto): Promise<UserResponseDto> {
         try {
-            const { email, username, password } = registerRequestDto;
+            const { email, username, password, friends } = registerRequestDto;
             const hashedPassword = await hash(password);
-            const user = await this.userModel.create({ email, username, password: hashedPassword });
-            const accessToken = this.jwtService.sign({ sub: user._id.toString() });
-            return new UserResponseDto(accessToken);
+            const newUser = new this.userModel({ email, username, password: hashedPassword, friends });
+            await newUser.save();
+            const accessToken = this.jwtService.sign({ sub: newUser._id.toString() });
+            return new UserResponseDto(accessToken, newUser.username);
         } catch (error) {
             if(error.message.includes('E11000 duplicate key error index')) {
                 throw new ConflictException('이미 존재하는 email 입니다.');
             } else {
+                console.log(error.message)
                 throw new InternalServerErrorException();
             }
         }
@@ -36,11 +42,12 @@ export class AuthService {
             const user = await this.userModel.findOne({ email }).exec();
             if (user && (await isHashValid(password, user.password))) {
                 const accessToken = this.jwtService.sign({ sub: user._id.toString() });
-                return { userResponseDto: new UserResponseDto(accessToken) };
+                return { userResponseDto: new UserResponseDto(accessToken, user.username) };
             } else {
                 throw new UnauthorizedException('login failed');
             }
         } catch (error) {
+            console.log(error.message)
             throw new Error(error.message);
         }
     }
@@ -48,4 +55,11 @@ export class AuthService {
         return await this.userModel.findOne({ _id: payload }).exec();
     }
     
+
+    getUserPopulated(userId: string) {
+        return this.userModel.findById(userId, { _id: 1, friends: 1 }).populate(
+            "friends",
+            "_id username mail"
+          );
+    }
 }
