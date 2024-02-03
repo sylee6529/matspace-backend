b97e85c729d7a6c5bcbd2ad33abd5190fd816c2e
sylee <sylee6529@gmail.com>
2024-02-03 21:16:39 +0900

    chore: 안 쓰는 import문 정리

--- src/room/room.service.ts
+++ src/room/room.service.ts
@@ -1,16 +1,9 @@
-import { Inject, Injectable, forwardRef } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { ConnectedSocket, SubscribeMessage } from '@nestjs/websockets';
-import { Model } from 'mongoose';
+import { Injectable, forwardRef } from '@nestjs/common';
+import { SubscribeMessage } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
-import { userDocument } from 'src/auth/user.schema';
 import { SocketService } from 'src/socket/socket.service';
-import { JwtStrategy } from 'src/auth/provider/jwt.strategy';
-import { UserSocket } from 'src/socket/interface/user.handshake';
 import { JwtService } from '@nestjs/jwt';
 import { AuthService } from 'src/auth/auth.service';
-import { SocketGateway } from 'src/socket/socket.gateway';
-import cryptoRandomString from 'crypto-random-string';
 
 @Injectable()
 export class RoomService {
