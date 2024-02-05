0cc05df0fc617dd9072e746751b6714be23c7f07
sylee <sylee6529@gmail.com>
2024-02-05 20:58:17 +0900

    feat: 음식 카테고리 선택 완료 추가

--- src/foodcategories/dto/post-speech-sentence.dto.ts
+++ src/foodcategories/dto/post-speech-sentence.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty } from "class-validator";
 
-export class PostMoodKeywordsDto {
+export class PostSpeechSentenceDto {
     @IsNotEmpty()
     @ApiProperty({ example: '오늘 한식이 먹고 싶어요', description: '음성 인식 문장' })
     readonly speechSentence: string;
--- src/foodcategories/foodcategories.controller.ts
+++ src/foodcategories/foodcategories.controller.ts
@@ -1,20 +1,33 @@
 import { Body, Controller, Post, Query, UseGuards } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUserId } from 'src/util/decorator/get-user.decorator';
-import { PostMoodKeywordsDto } from './dto/post-mood-keywords.dto';
+import { PostSpeechSentenceDto } from './dto/post-speech-sentence.dto';
+import { PostFoodCategoriesDto } from './dto/post-foodcategories.dto';
 
 @ApiTags('음식 카테고리 API')
 @Controller('api/foodcategories')
 export class FoodcategoriesController {
     @Post('/speech')
-    @ApiOperation({ summary: 'Post mood Keywords by user speech API', description: '유저가 말한 무드 키워드를 서버로 보냅니다.' })
-    @ApiCreatedResponse({ description: '무드 키워드가 잘 보내졌습니다.' })
+    @ApiOperation({ summary: 'Post food categories by user speech API', description: '유저가 말한 푸드 카테고리를 서버로 보냅니다.' })
+    @ApiCreatedResponse({ description: '푸드 카테고리가 잘 보내졌습니다.' })
     @UseGuards()
-    async getMoodKeywordsBySpeech(
+    async getfoodCategoriesBySpeech(
         @GetUserId() userId, 
         @Query() roomId: string, 
-        @Body() speechSentence: PostMoodKeywordsDto) {
+        @Body() speechSentence: PostSpeechSentenceDto) {
             // TODO: Fastapi로 request를 보내고, 받은 response를 다시 client에게 socket으로 보내기
+        return null;
+    }
+
+    @Post('')
+    @ApiOperation({ summary: 'Post a user\'s selected food categories API', description: '유저가 말한 푸드 카테고리를 서버로 보냅니다.' })
+    @ApiCreatedResponse({ description: '유저가 선택한 푸드 카테고리 전송 완료.' })
+    @UseGuards()
+    async selectMoodKeywords (
+        @GetUserId() userId, 
+        @Query() roomId: string, 
+        @Body() foodCategoriesDto: PostFoodCategoriesDto) {
+            // TODO: Room 인원 전원이 선택 완료하면, 모두에게 socket 보내기
         return null;
     }
 }
