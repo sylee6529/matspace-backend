1fb17f159c238d9f1a6174020310a1b13a308a1b
sylee <sylee6529@gmail.com>
2024-02-06 21:44:52 +0900

    refactor: 안쓰는 코드 삭제 (#21)

--- src/friend/dto/create.friend.invitation.dto.ts
+++
@@ -1,9 +0,0 @@
-// friend-invitation.dto.ts
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail } from 'class-validator';
-
-export class CreateFriendInvitationDto {
-  @IsEmail()
-  @ApiProperty({ example: 'abc123@ggg.com', description: '이메일' })
-  targetEmailAddress: string;
-}
